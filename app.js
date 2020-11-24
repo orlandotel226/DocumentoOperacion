cad05d93568b082a32ec9a14d3971d2c51f818a9
Orlando Cibrian <orlcibrrey12345@gmail.com>
2020-11-24 17:04:54 -0600

    Ultima modificacion de app.js

--- app.js
+++ app.js
@@ -1,15 +1,1 @@
-const sumar = (x, y) =>{
-	return x + y;
-}
-
-const restar = (x, y) =>{
-	return x - y;
-}
-
-const multiplicar = (x, y) =>{
-	return x * y;
-}
-
-const dividir = (x, y) =>{
-	return x / y;
-}
+console.log("Ing. Orlando Cibrian Reynoso");
